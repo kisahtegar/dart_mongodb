79f88e0bf84f9df7b01204cd12ff69db6efdca1f
kisah <data.kisahtegar@gmail.com>
2022-12-21 00:52:17 +0700

    First commit

---
+++ bin/dart_mongodb.dart
@@ -0,0 +1,5 @@
+import 'package:dart_mongodb/dart_mongodb.dart' as dart_mongodb;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_mongodb.calculate()}!');
+}
---
+++ test/dart_mongodb_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_mongodb/dart_mongodb.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
