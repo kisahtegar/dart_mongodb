be8c849b411146d1abe4b98fa3d4f9220343d3b6
kisah <data.kisahtegar@gmail.com>
2022-12-21 10:54:17 +0700

    API: Implement a RESTful API to interface with our database

--- bin/dart_mongodb.dart
+++ bin/dart_mongodb.dart
@@ -1,7 +1,12 @@
 // import 'package:dart_mongodb/dart_mongodb.dart' as dart_mongodb;
+import 'dart:convert';
+
 import 'package:mongo_dart/mongo_dart.dart';
+import 'dart:io';
 
 void main(List<String> arguments) async {
+  int port = 8085;
+  var server = await HttpServer.bind('localhost', port);
   // Connect to database
   Db db = Db('mongodb://127.0.0.1:27017/test');
   await db.open();
@@ -11,52 +16,117 @@ void main(List<String> arguments) async {
   // People Collection
   DbCollection peopleCollection = db.collection('people');
 
-  // Read people collection
-  // ---
-  // var people = await peopleCollection.find().toList();
-  // var people = await peopleCollection.find(where.eq('first_name', 'Calypso')).toList();
-  // var people = await peopleCollection.find(where.match('first_name', 'B')).toList();
-  var people = await peopleCollection.find(where.limit(5)).toList();
-  print("people: $people");
+  // Server listening
+  server.listen((HttpRequest request) async {
+    switch (request.uri.path) {
+      case '/':
+        request.response.write('Hello, world!');
+        await request.response.close();
+        break;
+      case '/people':
+        request.response
+          ..write(await peopleCollection.find().toList())
+          ..close();
 
-  var person = await peopleCollection
-      // .findOne(where.match('first_name', 'B').and(where.gt('id', 40)));
-      // .findOne(where.match('first_name', 'B').gt('id', 40));
-      .findOne(where.jsQuery('''
-      return this.first_name.startsWith('B') && this.id > 40;
-      '''));
-  print("person: $person");
+        // Handle GET request
+        if (request.method == 'GET') {
+          request.response.write(await peopleCollection.find().toList());
+        }
+        // Handle POST request
+        else if (request.method == 'POST') {
+          var content = await utf8.decoder.bind(request).join();
+          print(content);
+          var document = json.decode(content);
+          await peopleCollection.insertOne(document);
+        }
+        // Handle PUT request
+        else if (request.method == 'PUT') {
+          var id = int.parse(request.uri.queryParameters['id']!);
+          var content = await utf8.decoder.bind(request).join();
+          var document = json.decode(content);
+          var itemToReplace =
+              await peopleCollection.findOne(where.eq('id', id));
 
-  // Create person
-  await peopleCollection.insertOne({
-    "id": 101,
-    "first_name": "Jermaine",
-    "last_name": "Gippes",
-    "email": "cgippes2r@xinhuanet.com",
-    "gender": "Female",
-    "ip_address": "97.252.84.122"
+          if (itemToReplace == null) {
+            await peopleCollection.insertOne(document);
+          } else {
+            await peopleCollection.update(itemToReplace, document);
+          }
+        }
+        // Handle DELETE request
+        else if (request.method == 'DELETE') {
+          var id = int.parse(request.uri.queryParameters['id']!);
+          var itemToDelete = await peopleCollection.findOne(where.eq('id', id));
+          await peopleCollection.remove(itemToDelete);
+        }
+        // Handle PATCH request
+        else if (request.method == 'PATCH') {
+          var id = int.parse(request.uri.queryParameters['id']!);
+          var content = await utf8.decoder.bind(request).join();
+          var document = json.decode(content);
+          var itemToPatch = await peopleCollection.findOne(where.eq('id', id));
+          await peopleCollection.update(itemToPatch, {
+            r'$set': document,
+          });
+        }
+
+        await request.response.close();
+        break;
+      default:
+        request.response
+          ..statusCode = HttpStatus.notFound
+          ..write('Not Found');
+    }
   });
-  print("insertOne: saved new person");
-  print(await peopleCollection.findOne(where.eq('id', 101)));
 
-  // Update person
-  await peopleCollection.update(
-    await peopleCollection.findOne(where.eq('id', 101)),
-    {
-      r'$set': {
-        'gender': 'Male',
-      }
-    },
-  );
-  print("update: Updated person");
-  print(await peopleCollection.findOne(where.eq('id', 101)));
+  print("Server listening at http://localhost:$port");
 
-  // Delete person
-  await peopleCollection
-      .remove(await peopleCollection.findOne(where.eq('id', 101)));
-  print("remove: Deleted person");
-  print(await peopleCollection.findOne(where.eq('id', 101))); // null
+  // // Read people collection
+  // // ---
+  // // var people = await peopleCollection.find().toList();
+  // // var people = await peopleCollection.find(where.eq('first_name', 'Calypso')).toList();
+  // // var people = await peopleCollection.find(where.match('first_name', 'B')).toList();
+  // var people = await peopleCollection.find(where.limit(5)).toList();
+  // print("people: $people");
 
-  // Close Database
-  await db.close();
+  // var person = await peopleCollection
+  //     // .findOne(where.match('first_name', 'B').and(where.gt('id', 40)));
+  //     // .findOne(where.match('first_name', 'B').gt('id', 40));
+  //     .findOne(where.jsQuery('''
+  //     return this.first_name.startsWith('B') && this.id > 40;
+  //     '''));
+  // print("person: $person");
+
+  // // Create person
+  // await peopleCollection.insertOne({
+  //   "id": 101,
+  //   "first_name": "Jermaine",
+  //   "last_name": "Gippes",
+  //   "email": "cgippes2r@xinhuanet.com",
+  //   "gender": "Female",
+  //   "ip_address": "97.252.84.122"
+  // });
+  // print("insertOne: saved new person");
+  // print(await peopleCollection.findOne(where.eq('id', 101)));
+
+  // // Update person
+  // await peopleCollection.update(
+  //   await peopleCollection.findOne(where.eq('id', 101)),
+  //   {
+  //     r'$set': {
+  //       'gender': 'Male',
+  //     }
+  //   },
+  // );
+  // print("update: Updated person");
+  // print(await peopleCollection.findOne(where.eq('id', 101)));
+
+  // // Delete person
+  // await peopleCollection
+  //     .remove(await peopleCollection.findOne(where.eq('id', 101)));
+  // print("remove: Deleted person");
+  // print(await peopleCollection.findOne(where.eq('id', 101))); // null
+
+  // // Close Database
+  // await db.close();
 }
